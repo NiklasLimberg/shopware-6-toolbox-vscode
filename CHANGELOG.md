d3f07f0d7cd137ed47835acebe8656942a6ad528
Niklas Limberg <n.limberg@shopware.com>
2023-01-05 20:02:56 +0100

    Add initial README

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-All notable changes to the "shopware6-vscode-extension" extension will be documented in this file.
+All notable changes to the "shopware-6-toolbox" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,11 @@ import { getChangelogInBranch } from './commands/findChangelog';
 import { searchForChangelog } from './commands/searchForChangelog';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Extension "shopware6-vscode-extension" is now active!');
+    console.log('Extension "shopware-6-toolbox" is now active!');
 
     context.subscriptions.push(
-        vscode.commands.registerCommand('shopware6-vscode-extension.getChangelogInBranch', getChangelogInBranch),
-        vscode.commands.registerCommand('shopware6-vscode-extension.searchForChangelog', searchForChangelog),
+        vscode.commands.registerCommand('shopware-6-toolbox.getChangelogInBranch', getChangelogInBranch),
+        vscode.commands.registerCommand('shopware-6-toolbox.searchForChangelog', searchForChangelog),
     );
 }
 
